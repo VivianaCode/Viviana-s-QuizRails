a66dfa435b51291b778d579a1a8e0f78f82a81b6
Viviana <viviana.cristini@codemotion.it>
2014-09-20 18:05:41 +0200

    First commit

---
+++ app/controllers/front_end_controller.rb
@@ -0,0 +1,10 @@
+class FrontEndController < ApplicationController
+  def question
+    @quiz = Quiz.order('random()').first
+  end
+
+  def answer
+    @quiz = Quiz.find(params[:quiz_id])
+    @answer = Answer.find(params[:answer_id])
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuizRails::Application.config.secret_key_base = '9d7186018747bcd56238217d8cb4973f523a07f3214b32b26d5f1c31beed0e29ea0c485d2bdc4f92704d60b2308a74853d2f367dea654cf9ebb64923d5f6fccd'
